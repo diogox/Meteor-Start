292b2e7c4e71afd7408ef5024e688d02cf26a6d7
Diogo Xavier <diogoxavierpinto@gmail.com>
2017-07-23 15:39:41 +0100

    Add structure folders & mocha testing

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import AppRoutes from '../imports/routes/AppRoutes';
+import '../imports/startup/simpl-schema-configuration.js';
 
 Meteor.startup(() => {
   ReactDom.render(AppRoutes, document.getElementById('app'));
